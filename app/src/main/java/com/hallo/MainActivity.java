7188c9a2bebc136c19843d10e6afeec63b34e063
Xun <siaz0002@e.edu.ntu.sg>
2019-02-16 17:48:52 +0800

    working on the Settings tab

--- app/src/main/java/com/hallo/MainActivity.java
+++ app/src/main/java/com/hallo/MainActivity.java
@@ -8,35 +8,38 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.util.Log;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
     public static String PARAM_PHONE_NUMBER = "PHONE_NUMBER";
     public static String PARAM_REMEMBER_PHONE_NUMBER = "REMEMBER_PHONE_NUMBER";
     Context mcontext;
+    Fragment selectedFragment = null;
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            Fragment selectedFragment = null;
             switch (item.getItemId()) {
                 case R.id.navigation_home:
                     selectedFragment = HomeFragment.newInstance();
+                    displayFragment(selectedFragment);
                     return true;
                 case R.id.navigation_reminder:
                     selectedFragment = ReminderFragment.newInstance();
+                    displayFragment(selectedFragment);
                     return true;
                 case R.id.navigation_todo:
                     selectedFragment = ToDoListFragment.newInstance();
+                    displayFragment(selectedFragment);
                     return true;
                 case R.id.navigation_settings:
                     selectedFragment = SettingsFragment.newInstance();
+                    displayFragment(selectedFragment);
                     return true;
             }
-            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-            transaction.replace(R.id.frame_layout, selectedFragment);
-            transaction.commit();
             return false;
         }
     };
@@ -56,7 +59,17 @@ public class MainActivity extends AppCompatActivity {
             startActivity(intent);
         } else {
             //load home page
+            if(savedInstanceState == null) {
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                transaction.replace(R.id.frame_layout, HomeFragment.newInstance());
+                transaction.commit();
+            }
         }
     }
 
+    protected void displayFragment(Fragment fragment){
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_layout, fragment);
+        transaction.commit();
+    }
 }
--- app/src/main/java/com/hallo/SettingsFragment.java
+++ app/src/main/java/com/hallo/SettingsFragment.java
@@ -1,5 +1,6 @@
 package com.hallo;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -7,21 +8,85 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import java.util.Locale;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.util.DisplayMetrics;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemSelectedListener;
 
 public class SettingsFragment extends Fragment {
 
-    public static ToDoListFragment newInstance(){
-        ToDoListFragment fragment = new ToDoListFragment();
+    Spinner spinnerctrl;
+    Button btn;
+    Locale myLocale;
+
+    public static SettingsFragment newInstance(){
+        SettingsFragment fragment = new SettingsFragment();
         return fragment;
     }
-    @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        spinnerctrl = (Spinner) view.findViewById(R.id.spinner1);
+        spinnerctrl.setOnItemSelectedListener(new OnItemSelectedListener() {
+
+            public void onItemSelected(AdapterView<?> parent, View view,
+                                       int pos, long id) {
+
+                if (pos == 1) {
+
+                    Toast.makeText(parent.getContext(),
+                            "You have selected English", Toast.LENGTH_SHORT)
+                            .show();
+                    setLocale("ta");
+                } else if (pos == 2) {
+
+                    Toast.makeText(parent.getContext(),
+                            "You have selected Chinese", Toast.LENGTH_SHORT)
+                            .show();
+                    setLocale("hi");
+                } else if (pos == 3) {
+
+                    Toast.makeText(parent.getContext(),
+                            "You have selected Malay", Toast.LENGTH_SHORT)
+                            .show();
+                    setLocale("en");
+                }
+
+            }
+
+            public void onNothingSelected(AdapterView<?> arg0) {
+                // TODO Auto-generated method stub
+            }
+
+        });
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.todolist_layout, container, false);
+        return inflater.inflate(R.layout.settings_layout, container, false);
     }
+
+    public void setLocale(String lang) {
+
+        myLocale = new Locale(lang);
+        Context mcontext = getContext();
+        Resources res = getResources();
+        DisplayMetrics dm = res.getDisplayMetrics();
+        Configuration conf = res.getConfiguration();
+        conf.locale = myLocale;
+        res.updateConfiguration(conf, dm);
+        Intent refresh = new Intent(mcontext, SettingsFragment.class);
+        startActivity(refresh);
+    }
+
 }
